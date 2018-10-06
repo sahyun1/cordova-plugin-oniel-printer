a4807795ab80932deb95d6f98c81a9c08c415254
MeGaT <megat@MacBookPro.local>
2018-10-06 17:11:42 +0800

    first commit

---
+++ www/OnielPrinter.js
@@ -0,0 +1,31 @@
+var exec = require('cordova/exec');
+var OnielPrinter = {
+    platforms: ['android'],
+
+    isSupported: function () {
+        if (window.device) {
+            var platform = window.device.platform;
+            if ((platform !== undefined) && (platform !== null)) {
+                return (this.platforms.indexOf(platform.toLowerCase()) >= 0);
+            }
+        }
+        return false;
+    },
+
+    connect: function (onSuccess, onError) {
+        exec(onSuccess, onError, 'OnielPrinter', 'connect');
+    },
+    disconnect: function (onSuccess, onError) {
+        exec(onSuccess, onError, 'OnielPrinter', 'disconnect', []);
+    },
+    printText: function (text, onSuccess, onError) {
+        exec(onSuccess, onError, 'OnielPrinter', 'printText', [text]);
+    },
+    printBarcode: function (type, data, onSuccess, onError) {
+        exec(onSuccess, onError, 'OnielPrinter', 'printBarcode', [type, data]);
+    },
+    printImage: function (image, width, height, align, onSuccess, onError) {
+        exec(onSuccess, onError, 'OnielPrinter', 'printImage', [image, width, height, align]);
+    },
+};
+module.exports = OnielPrinter;
