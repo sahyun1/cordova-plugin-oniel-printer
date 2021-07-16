03714415ea06db93c693905f9d8188e975f2fe67
Alston Kim <alston.kim@complypro.co.nz>
2021-07-16 12:08:14 +1200

    error callback is returned regardless of connection

--- src/android/OnielPrinter.java
+++ src/android/OnielPrinter.java
@@ -343,9 +343,9 @@ public class OnielPrinter extends CordovaPlugin {
     } catch (Exception e) {
       if(conn != null) {
         conn.close();
-        e.printStackTrace();
-        callbackContext.error("unable to print");
       }
+       e.printStackTrace();
+       callbackContext.error("unable to print");
     }
 
     Runnable progressRunnable = new Runnable() {
